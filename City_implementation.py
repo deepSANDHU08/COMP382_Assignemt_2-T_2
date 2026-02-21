1dcad4367422bd70a94b446882b95099eea5f3d7
Pushpdeep.Sandhu <Pushpdeep.sandhu@student.ufv.ca>
2026-02-20 20:27:29 -0800

    Make_Stack is ready, please review

--- City_implementation.py
+++ City_implementation.py
@@ -9,7 +9,7 @@ City = List[List(Stack)]  # this is for 2D grid : city[y][x]
 
 #start with basic parameter first
 @dataclass
-class CityParams
+class CityParams:
     seed: int = 42
 
     #chance that a grid cell has no building
@@ -25,3 +25,28 @@ class CityParams
 def random_color(rng: random.Random) -> Color:
     return rng.choice(["R", "G", "B"])
 
+def make_stack(rng: random.Random, params: CityParams) -> Satck:
+    """
+    Creates one buikding stack.
+
+    Height is random between min_height and max_height,
+    then we fill it random cube colors.
+    """
+    height = rng.randint(params.min_height, params.max_heigth)
+
+    stack = []
+    for _ in range(height):
+     stack.appenf(random_color(rng))
+
+    return stack
+## syntax errors fixed remember the intedentation for next time
+
+def generate_city(width: int, height: int, params: CityParams) -> City:
+   # nested for loops for range in height and range in width
+   # try to do it 3D, brains stroms some idea.
+
+def print_height_map():
+   # this one is for visulization
+
+# REMEMBER TO MAKE A TEMPORARY MAIN HERE FOR TESTING 
+# THINK ABOUT RANDOM VARAIBLES AND PROPBABBILIES OF COLORS
